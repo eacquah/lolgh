66bb19775c6a01060e2436520d86c50084cc5327
manny <manny.acquah@gmail.com>
2014-09-19 01:12:28 +0100

    Minor edit to template and config restructure

--- config.php
+++ config.php
@@ -31,12 +31,3 @@ include('lib/Comic.php');
 include('lib/Db.php');
 include('lib/Dao.php');
 
-
-//$MyPdo = new \classes\MyPDO('db/db.sqlite');
-$user = new \Lib\User();
-$db   = new \Lib\Db($dbFile);
-$dao  = new \Lib\Dao();
-$dao->setDb($db);
-$allComics = $dao->fetchAll('comic');
-
-echo $twig->render('index.html', array('the' => 'variables', 'go' => 'here'));
