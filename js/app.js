125f95872e721e25b58472a0cdeb2fd802e0b625
Manny <manny.acquah@twentysixdigital.com>
2014-10-09 14:00:47 +0100

    styling changes. template changes. dao update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function() {
+    if (!Modernizr.cssvhunit) {
+        var windowH = $(window).height();
+        $('.fb-like-box').css({'height':($(window).height())+'px'});
+    }
+});
