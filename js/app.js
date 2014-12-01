851150f155c2251b28134d8c1e0c79e7284b2122
Manny <manny.acquah@twentysixdigital.com>
2014-12-01 16:54:42 +0000

    Built modal download button for android app

--- js/app.js
+++ js/app.js
@@ -8,15 +8,28 @@ $(function() {
         $('.fb-like-box').css({'height':($(window).height())+'px'});
     }
 
+    var ua = navigator.userAgent.toLowerCase();
+    var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+    //if(isAndroid && (typeof $.cookie('android') === 'undefined' || $.cookie('android') === null)) {
+    if(!isAndroid) {
+        // Show download app modal!
+        $('#download-app-modal').foundation('reveal', 'open');
+        $.cookie('android', 1, { expires: 7 });
+    }
+
+    $('a.custom-close-reveal-modal').click(function(){
+        $('#download-app-modal').foundation('reveal', 'close');
+    });
+
     // Bind the swipeHandler callback function to the swipe event on div.box
-    $( "div#main-comic" ).on( "swipe", swipeHandler );
+    //$( "div#main-comic" ).on( "swipe", swipeHandler );
 
     // Callback function references the event target and adds the 'swipe' class to it
-    function swipeHandler( event ){
+    /*function swipeHandler( event ){
         //alert('test');
     }
 
-    /*$('#container').imagesLoaded( function(){
+    $('#container').imagesLoaded( function(){
         $('#container').masonry({
             itemSelector: '.item',
             isAnimated: true,
